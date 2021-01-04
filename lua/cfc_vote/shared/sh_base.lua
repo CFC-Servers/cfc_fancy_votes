321a1e55911142714473922ce9bd1a37d829a343
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2021-01-04 13:39:37 -0500

    Improve visuals

--- lua/cfc_vote/shared/sh_base.lua
+++ lua/cfc_vote/shared/sh_base.lua
@@ -11,7 +11,7 @@ end
 util.AddNetworkString( CFC_Vote.NET_LIVE_UPDATE )
 
 CFC_Vote.VOTE_COMMAND = CreateConVar( "cfc_vote_chat_command", "!cfcvote", FCVAR_NONE, "Chat command for CFC Vote" )
-CFC_Vote.VOTE_MAX_OPTIONS = CreateConVar( "cfc_vote_max_options", 6, FCVAR_NONE, "Max number of vote options", 2, 10 )
+CFC_Vote.VOTE_MAX_OPTIONS = CreateConVar( "cfc_vote_max_options", 10, FCVAR_NONE, "Max number of vote options", 2, 10 )
 CFC_Vote.VOTE_DURATION = CreateConVar( "cfc_vote_duration", 30, FCVAR_NONE, "How long votes will last for, in seconds", 1, 50000 )
 CFC_Vote.RESULTS_DURATION = CreateConVar( "cfc_vote_results_duration", 15, FCVAR_NONE, "How long vote results will last for, in seconds", 1, 50000 )
 CFC_Vote.NOTIFICATION_VOTE_NAME = "CFC_Vote_VoteQuery"
