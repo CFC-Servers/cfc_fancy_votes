6aeca382c6b6f71456291adb98ca99359cb11d94
legokidlogan <legokidlogan@gmail.com>
2021-03-07 12:29:07 -0700

    Utilize new built-in button edit funcs

--- lua/cfc_vote/client/net.lua
+++ lua/cfc_vote/client/net.lua
@@ -1,23 +1,3 @@
-net.Receive( CFC_Vote.NET_LIVE_UPDATE, function()
-    for _, popup in pairs( CFCNotifications._popups ) do
-        local notif = popup.notification
-
-        if notif._id == CFC_Vote.NOTIFICATION_LIVE_NAME then
-            local buttonPanels = notif._btns
-            local index = net.ReadInt( 9 )
-            local count = net.ReadInt( 9 )
-            local nonresponseIndex = net.ReadInt( 9 )
-            local nonresponseCount = net.ReadInt( 9 )
-            local option = net.ReadString()
-
-            buttonPanels[index]:SetText( option .. "\n" .. count )
-            buttonPanels[nonresponseIndex]:SetText( "No Response\n" .. nonresponseCount )
-
-            break
-        end
-    end
-end )
-
 net.Receive( CFC_Vote.NET_CONSOLE_PRINT, function()
     MsgN( net.ReadString() )
 end )
